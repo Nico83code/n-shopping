ae2531e95b61754c66f1179baa3f01a99c52af9c
Nico83code <nicoman@gmail.com>
2020-09-24 16:24:54 +0200

    basket remove option added

--- src/App.js
+++ src/App.js
@@ -10,8 +10,11 @@ function App() {
     <Router>
       <div className="app">
         <Header />
-        
+
         <Switch>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
           <Route path="/checkout">
             <Checkout />
           </Route>
