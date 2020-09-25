118ffdd45aaeac72308929d45b91da443963bd8c
Nico83code <nicoman@gmail.com>
2020-09-25 16:47:11 +0200

    login and register fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Checkout from './Components/Checkout';
+import Login from './Components/Login'
 
 function App() {
+  useEffect(() => {
+
+  }, [])
+
   return (
     <Router>
       <div className="app">
-        <Header />
-
         <Switch>
           <Route path="/login">
-            <h1>Login Page</h1>
+            <Login />
           </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDlCMYKeev2Iv4DLrQhY2CF0lh4d-bGEew",
     authDomain: "n-shopping.firebaseapp.com",
@@ -8,3 +10,10 @@ const firebaseConfig = {
     appId: "1:473955149595:web:217473cf0c71f805ea95be",
     measurementId: "G-31HG6S4HFD"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
