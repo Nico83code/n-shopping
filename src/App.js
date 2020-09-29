07093f66d0f3486a0534a122e944246200748e55
Nico83code <nicoman@gmail.com>
2020-09-29 14:40:13 +0200

    login with name

--- src/App.js
+++ src/App.js
@@ -5,10 +5,31 @@ import Home from './Components/Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Checkout from './Components/Checkout';
 import Login from './Components/Login'
+import { auth } from "./firebase"
+import { useStateValue } from './StateProvider';
 
 function App() {
-  useEffect(() => {
+  const [{ }, dispatch] = useStateValue();
 
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>>', authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was loggedin
+        dispatch({
+          type: "SET_USER",
+          user: authUser
+        })
+
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null
+        })
+      }
+    })
   }, [])
 
   return (
--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -5,13 +5,14 @@ import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from '../StateProvider';
 
 function Checkout() {
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
 
     return (
         <div className="checkout">
             <div className="checkout__left">
                 <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout__ad" />
                 <div>
+                    <h3>{user?.email}</h3>
                     <h2 className="checkout__title">Your shopping Basket</h2>
 
                     {basket.map(item => (
