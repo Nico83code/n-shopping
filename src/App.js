256ca477c604e253819597619c2a451cb0605507
Nico83code <nicoman@gmail.com>
2020-10-11 13:10:13 +0200

    footer navbar added

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ function App() {
 
         </Switch>
       </div>
-    </Router>
+    </Router >
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import './Navbar.css'
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 
 function Navbar() {
     return (
-        <div className="Navbar__container">
-            <div className="Navbar__section">
-                <LocationOnIcon />
+        <div className="navbar__container">
+            <div>
+                <LocationOnOutlinedIcon className="navbar_location" />
+            </div>
+            <div className="navbar__section">
+                <span className="navbar__optionLineOne">Hello</span>
+                <span className="navbar__optionLineTwo">Select your address</span>
+            </div>
 
-
+            <div className="navbar__menu">
+                <ul>
+                    <li>Best Sellers</li>
+                    <li>Today's Deals</li>
+                    <li>Prime Video</li>
+                    <li>Customer Service</li>
+                    <li>Books</li>
+                    <li>New Releases</li>
+                    <li>Gift Ideas</li>
+                    <li>Home & Garden</li>
+                    <li>Electronics</li>
+                </ul>
             </div>
         </div>
     )
